7b6b86311538ce1f2a0042bd28751ab12ae29b1a
Andrey Felipe Cubas Cardoso <felipedreycubascardoso@gmail.com>
2025-12-17 21:32:02 -0300

    feat: adicionar componente InputFloat com suporte a rótulo flutuante

---
+++ app/components/inputFloat/InputFloat.tsx
@@ -0,0 +1,54 @@
+import React, { useId } from "react";
+import Input from "../input/Input";
+
+
+type InputFloatProps =  {
+  label: string;
+  containerClassName?: string;
+}& Omit<React.ComponentProps<"input">, "className">;
+
+
+export const InputFloat = React.forwardRef<
+  HTMLInputElement,
+  InputFloatProps
+>(({ label, id, containerClassName, ...inputProps }, ref) => {
+
+  const internalId = useId();
+  const effectiveId = id || internalId;
+
+  return (
+    <div className={`relative w-full ${containerClassName || ""}`}>
+      
+      <Input
+        {...inputProps}
+        ref={ref}
+        id={effectiveId}
+        placeholder=" "
+      />
+
+      <label
+        htmlFor={effectiveId}
+        className="
+          absolute left-3 top-1/2 -translate-y-1/2 
+          bg-white px-1 text-gray-500 pointer-events-none
+          transition-all duration-200 
+
+          peer-placeholder-shown:text-base
+          peer-placeholder-shown:top-1/2
+          peer-placeholder-shown:-translate-y-1/2
+
+          peer-focus:top-0
+          peer-focus:-translate-y-1/2
+          peer-focus:text-xs
+          peer-focus:text-blue-600
+
+          peer-not-placeholder-shown:top-0
+          peer-not-placeholder-shown:-translate-y-1/2
+          peer-not-placeholder-shown:text-xs
+        "
+      >
+        {label}
+      </label>
+    </div>
+  );
+});
