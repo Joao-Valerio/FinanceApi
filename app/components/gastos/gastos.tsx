c91b6be002c14adbb2fccb74ab1fa6829e98e8e8
João Valério Santana <151927936+ohomiebrabo@users.noreply.github.com>
2026-01-05 16:46:48 -0300

    style: melhorando sidebar

--- app/components/gastos/gastos.tsx
+++ app/components/gastos/gastos.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router';
+import { Sidebar } from './gastosSidebar';
 import { CartesianGrid, Line, LineChart, XAxis } from "recharts";
 import type { ChartConfig } from "../ui/chart";
 import type { ColumnDef } from "@tanstack/react-table";
@@ -254,17 +254,14 @@ const Gastos = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className="relative min-h-screen dark:bg-gray-900">
-      
-      {/* Botão do menu fixo no topo — só aparece quando sidebar está fechada */}
-      {!sidebarOpen && (
-      <button
+    <div className="min-h-screen bg-white dark:bg-gray-900">
+            <button
         onClick={() => setSidebarOpen(!sidebarOpen)}
         className="m-4 p-2 text-3xl text-black rounded hover:bg-green-700 transition dark:text-white"
       >
         ☰
       </button>
-      )}
+
 
       {sidebarOpen && (
         <div
@@ -273,33 +270,8 @@ const Gastos = () => {
         />
       )}
 
-      <div
-        className={`fixed top-0 left-0 h-full w-64 bg-gray-400 dark:bg-gray-800 text-black dark:text-white p-6 shadow-lg transform transition-transform duration-300 z-20 ${
-          sidebarOpen ? "translate-x-0" : "-translate-x-full"
-        }`}
-      >
-        <h2 className="text-4xl font-bold mb-6">Painel</h2>
-        <ul className="space-y-4">
-          <li><Link to="/dashboard" className="hover:text-green-400 font-semibold">Dashboard</Link></li>
-          <li><Link to="/gastos" className="hover:text-green-400 font-semibold">Gastos</Link></li>
-          <li><Link to="/metas" className="hover:text-green-400 font-semibold">Metas</Link></li>
-          <li><Link to="/relatorios" className="hover:text-green-400 font-semibold">Relatórios</Link></li>
-          <li><Link to="/recursos" className="hover:text-green-400 font-semibold">Recursos</Link></li>
-          <li><Link to="/sobre" className="hover:text-green-400 font-semibold">Sobre</Link></li>
-          <li><Link to="/contato" className="hover:text-green-400 font-semibold">Contato</Link></li>
-          <li><Link to="/" className="hover:text-red-700 font-semibold">Sair</Link></li>
-        </ul>
-        <div className="absolute bottom-6 left-6 flex items-center space-x-4">
-          <img
-            src="https://i.pravatar.cc/100?u=joao"
-            alt="Foto de Joao"
-            className="w-10 h-10 rounded-full border-2 border-green-500"
-          />
-          <div>
-            <Link to="/"><p className="text-sm font-semibold">Joao</p></Link>
-          </div>
-        </div>
-      </div>
+
+      <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
 
 <div className="relative gap-8 ">
   <main className="mt-4 px-4 sm:px-20 ">
