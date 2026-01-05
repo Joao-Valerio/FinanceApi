ea211f763b892f0aed1750c737a23d13885f2d1c
João Valério Santana <151927936+ohomiebrabo@users.noreply.github.com>
2026-01-05 15:58:12 -0300

    style: melhorando a sidebar

--- app/components/dashboard/dashboard.tsx
+++ app/components/dashboard/dashboard.tsx
@@ -1,7 +1,8 @@
-import { Link } from "react-router";
 import { useEffect , useState } from "react";
 import { Area, AreaChart, CartesianGrid, XAxis } from "recharts"
 import type { ChartConfig } from "../ui/chart"
+import { Sidebar } from "./dashboardSidebar";
+
 
 import {
   Card,
@@ -288,14 +289,13 @@ const timeRangeLabels: Record<string, string> = {
   );
 }
 
-export const Dashboard = () => {
+export const Dashboard: React.FC = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
 
   return (
-    <div className="relative min-h-screen dark:bg-gray-900">
-
-      <button
+    <div className="min-h-screen bg-white dark:bg-gray-900">
+            <button
         onClick={() => setSidebarOpen(!sidebarOpen)}
         className="m-4 p-2 text-3xl text-black rounded hover:bg-green-700 transition dark:text-white"
       >
@@ -310,36 +310,8 @@ export const Dashboard = () => {
         />
       )}
 
-      <div
-        className={`fixed top-0 left-0 h-full w-64 bg-gray-400 dark:bg-gray-800 text-black dark:text-white p-6 shadow-lg transform transition-transform duration-300 z-20 ${
-          sidebarOpen ? "translate-x-0" : "-translate-x-full"
-        }`}
-      >
-        <h2 className="text-4xl font-bold mb-6">Painel</h2>
-        <ul className="space-y-4">
-            <li><Link to="/dashboard" className="hover:text-green-400 font-semibold">Dashboard</Link></li>
-            <li><Link to="/gastos" className="hover:text-green-400 font-semibold">Gastos</Link></li>
-            <li><Link to="/metas" className="hover:text-green-400 font-semibold">Metas</Link></li>
-            <li><Link to="/relatorios" className="hover:text-green-400 font-semibold">Relatórios</Link></li>
-            <li><Link to="/recursos" className="hover:text-green-400 font-semibold">Recursos</Link></li>
-            <li><Link to="/sobre" className="hover:text-green-400 font-semibold">Sobre</Link></li>
-            <li><Link to="/contato" className="hover:text-green-400 font-semibold">Contato</Link></li>
-            <li><Link to="/" className="hover:text-red-700 font-semibold">Sair</Link></li>
-        </ul>
 
-        <div className="absolute bottom-6 left-6 flex items-center space-x-4">
-          <img
-            src="https://i.pravatar.cc/100?u=joao"
-            alt="Foto de Joao"
-            className="w-10 h-10 rounded-full border-2 border-green-500"
-          />
-          <div>
-            <Link to="/">
-              <p className="text-sm font-semibold">Joao</p>
-            </Link>
-          </div>
-        </div>
-      </div>
+      <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
 
       <main className="p-8 sm:p-6 md:p-8 grid grid-cols-1 md:grid-cols-3 gap-6 text-white ">
       {/* 💰 Saldo do usuário */}
